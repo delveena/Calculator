49d27ba5869ddd4ea0dd9fda495c486ba52416bd
delveenalhamka <delveenda@gmail.com>
2019-02-17 19:53:31 -0700

    cse360assign2, second version of Calculator class.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -19,51 +19,68 @@ package cse360assign2;
 			/**
 			 * public method that will calculate total
 			 */
-			public Calculator () {
+			public Calculator () 
+			{
 				total = 0;  // not needed - included for clarity
 			}
 			/**
 			 * getTotal method that will return calculated total
 			 * @return method that wil get calculated total of the calculator 
 			 */
-			public int getTotal () {
-				return 0;
+			public int getTotal () 
+			{
+				return total;
 			}
 			/**
 			 * This method is used to add an integer(s) 
 			 * @param value This is a parameter of the add method
 			 * 
 			 */
-			public void add (int value) {
+			public void add (int value) 
+			{
+				total += value;
 				
 			}
 			/**
 			 * this method is used to subtract an integer or integers 
 			 * @param value This is a parameter of the subtract method
 			 */
-			public void subtract (int value) {
-				
+			public void subtract (int value) 
+			{
+				total -= value;
 			}
 			/**
 			 * This method is used to multiply an integer or integers
 			 * @param value This is a parameter of the multiply method
 			 */
-			public void multiply (int value) {
-				
+			public void multiply (int value) 
+			{
+				total *= value;
 			}
 			/**
 			 * This method is used to divide an integer or integers
 			 * @param value This is a parameter of the divide method
 			 */
-			public void divide (int value) {
+			public void divide (int value) 
+			{
+				if (value == 0)
+					{
+						total = 0;
+					}
+				else 
+					{
+						total /= value;
+					}
+				}
 				
-			}
+			
 			/**
 			 * This string get method will get the history of calculator 
 			 * @return This string will return the history,
 			 * starting from initial value all the way through to the last value 
 			 */
-			public String getHistory () {
+			public String getHistory () 
+			{
 				return "";
 			}
 		}
